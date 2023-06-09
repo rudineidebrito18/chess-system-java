17901f65724d1a96d3e88b800a59d09bfdbcc4cd
Rudinei de Brito <rudineidebrito18@gmail.com>
2023-06-09 08:57:42 -0300

    special move casting

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -2,18 +2,30 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class King extends ChessPiece {
 
-    public King(Board board, Color color) {
+    private ChessMatch chessMatch;
+
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     private boolean canMove(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p == null || p.getColor() != getColor();
+    }
+
+    private boolean testRookCasting(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        if (p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0) {
+            return true;
+        }
+        return false;
     }
 
     @Override
@@ -61,6 +73,27 @@ public class King extends ChessPiece {
         p.setValues(position.getRow() + 1, position.getColumn() - 1);
         if (getBoard().positionExists(p) && canMove(p)) {
             mat[p.getRow()][p.getColumn()] = true;
+        }
+
+        //move casting
+        if (getMoveCount() == 0 && !chessMatch.isCheck()) {
+            Position posRook1 = new Position(position.getRow(),position.getColumn() + 3);
+            if (testRookCasting(posRook1)) {
+                Position p1 = new Position(position.getRow(),position.getColumn() + 1);
+                Position p2 = new Position(position.getRow(),position.getColumn() + 2);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2) == null) {
+                    mat[position.getRow()][position.getColumn() + 2] = true;
+                }
+            }
+            Position posRook2 = new Position(position.getRow(),position.getColumn() - 4);
+            if (testRookCasting(posRook1)) {
+                Position p1 = new Position(position.getRow(),position.getColumn() - 1);
+                Position p2 = new Position(position.getRow(),position.getColumn() - 2);
+                Position p3 = new Position(position.getRow(),position.getColumn() - 3);
+                if (getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null) {
+                    mat[position.getRow()][position.getColumn() - 2] = true;
+                }
+            }
         }
 
         return mat;
